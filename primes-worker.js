7df7560aac2e51b888cc4e5fa39cbe65574b1d6a
Lucas Garron <lucasg@gmx.de>
2012-01-27 17:29:18 -0800

    Simple web worker example.

---
+++ primes-worker.js
@@ -0,0 +1,35 @@
+
+this.addEventListener('message', function(e) {
+
+  var max = e.data;
+  var numPrimesUnder = primesUnder(max);
+  this.postMessage('Number of primes under ' + max + ': ' + numPrimesUnder);
+
+}, false);
+
+
+var isPrime = function(n) {
+  
+  for (var i = 2; i < n; i++) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+
+var primesUnder = function(max) {
+
+  var total = 0;
+
+  for (var i = 2; i < max; i++) {
+    if (isPrime(i)) {
+      total++
+    }
+  }
+
+  return total;
+}
+
